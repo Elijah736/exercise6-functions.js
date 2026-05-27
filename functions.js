21e6ad02e7aebadb914aa48f84333973916085d2
Elijah Lategan <elijahlategan3@gmail.com>
2026-05-27 14:28:04 +0200

    intial commit

---
+++ functions.js
@@ -0,0 +1,46 @@
+// Done by Lukho
+// Create a function called greet that takes a name parameter
+function greet(name) {
+    // TODO: Make the greet function return "Hello, [name]!"
+    return "Hello, " + name + "!";
+}
+
+let x = greet("Lukho")
+console.log(x)
+
+
+
+// Done by Elijah
+// TODO: Create a function called add that takes two parameters
+function add(num1, num2){
+    let x = num1 + num2;
+    // TODO: Make the add function return the sum of both parameters
+    return x;
+}
+let i = add(5, 10);
+console.log(`The answer is ${i}`)
+
+
+
+
+// Done by ishma-iel
+// Function 1
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Function 2
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Call both functions and store their results in variables
+let additionResult = addNumbers(5, 3);
+let multiplicationResult = multiplyNumbers(5, 3);
+
+// Print the results to the console
+console.log("Addition Result:", additionResult);
+console.log("Multiplication Result:", multiplicationResult)
+
+
+
